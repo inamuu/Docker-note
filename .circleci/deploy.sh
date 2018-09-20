30f448fa5b9f9e1c577c9fe7eb334106fcc702eb
inamuu <kzm0211@gmail.com>
2018-09-20 20:28:44 +0900

    :+1: タイムアウト値を伸ばす

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,4 +2,4 @@
 
 eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/inamuu-test01:$CIRCLE_SHA1
-./ecs-deploy -r ap-northeast-1 --cluster inamuu-cluster1 --service-name inamuu-testapp --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/inamuu-test01:$CIRCLE_SHA1
+./ecs-deploy -r ap-northeast-1 --cluster inamuu-cluster1 --service-name inamuu-testapp --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/inamuu-test01:$CIRCLE_SHA1 -t 300
