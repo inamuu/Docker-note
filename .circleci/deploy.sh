d7a8d8214b6310c00bde975af6b9e7a87b15f87b
inamuu <kzm0211@gmail.com>
2018-09-20 20:13:45 +0900

    :bug: ECRのログイン対策
    ref: https://qiita.com/nagais/items/b60824274d9ca724dea7

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-eval $(aws ecr get-login --region ap-northeast-1)
+eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/inamuu-test01:$CIRCLE_SHA1
 ./ecs-deploy --cluster inamuu-cluster1 --service-name inamuu-testapp --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/inamuu-test01:$CIRCLE_SHA1
